9faded926f3f2d6930bbe06c7d0e91714a0871c0
Ian Ross <ianrossdrums@gmail.com>
2022-05-23 15:53:30 -0600

    feat: add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :user_parties
   has_many :parties, through: :user_parties
+
+  validates_presence_of :name
+  validates_uniqueness_of :email
+  has_secure_password
 end
